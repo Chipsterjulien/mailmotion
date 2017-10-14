12e0cc5fd4284df2f19a04a2b445423fcb16035a
Julien Freyermuth <julien.chipster@gmail.com>
2017-10-14 15:22:39 +0200

    Replace a lot of code in order to increase performance

--- app.go
+++ app.go
@@ -10,23 +10,24 @@ import (
 	"time"
 
 	"bitbucket.org/zombiezen/cardcpx/natsort"
+
 	"github.com/jordan-wright/email"
-	"github.com/op/go-logging"
+	logging "github.com/op/go-logging"
 	"github.com/spf13/viper"
 )
 
-type piece struct {
-	Email   *email.Email
-	Picture string
+type attachment struct {
+	email       *email.Email
+	pictureName string
 }
 
 func main() {
-	confPath := "/etc/mailmotion/"
+	confPath := "/etc/mailmotion"
 	confFilename := "mailmotion"
 	logFilename := "/var/log/mailmotion/error.log"
 
-	// confPath := "cfg/"
-	// confFilename := "mailmotion"
+	// confPath := "cfg"
+	// confFilename := "mailmotion_sample"
 	// logFilename := "error.log"
 
 	fd := initLogging(&logFilename)
@@ -36,18 +37,13 @@ func main() {
 	startApp()
 }
 
-func changePathList(pictureList *[]string, newPath *string) {
-	log := logging.MustGetLogger("log")
-
-	for num, pic := range *pictureList {
-		_, filename := path.Split(pic)
-		(*pictureList)[num] = path.Join(*newPath, filename)
-
-		log.Debugf("\"%s\" was changed to \"%s\"", pic, (*pictureList)[num])
+func addUpperExtension(extensionListPtr *[]string) {
+	for _, ext := range *extensionListPtr {
+		*extensionListPtr = append(*extensionListPtr, strings.ToUpper(ext))
 	}
 }
 
-func createEmail(pic *string) *email.Email {
+func createAnEmail(pic *string) *email.Email {
 	e := email.NewEmail()
 	e.From = viper.GetString("email.from")
 	e.To = viper.GetStringSlice("email.sendTo")
@@ -61,7 +57,7 @@ func createPath(newPath *string) {
 	log := logging.MustGetLogger("log")
 
 	if err := os.MkdirAll(*newPath, 0744); err != nil {
-		log.Criticalf("Unable to create directories: %s", err)
+		log.Criticalf("Unable to create \"%s\" directorie(s): %s", *newPath, err)
 		os.Exit(1)
 	}
 
@@ -71,67 +67,33 @@ func createPath(newPath *string) {
 func findPicture() []string {
 	log := logging.MustGetLogger("log")
 
-	path := viper.GetString("default.picturePath")
+	picturePath := viper.GetString("default.picturePath")
+	pictureExtList := viper.GetStringSlice("default.pictureExt")
+	picturesList := []string{}
 
-	types := []string{"*.jpg", "*.JPG", "*.ppm", "*.PPM"}
-	pictureList := []string{}
+	addUpperExtension(&pictureExtList)
 
-	log.Debugf("Pictures extensions: %v", types)
-	log.Debugf("Picture path: %s", path)
-
-	for _, ext := range types {
-		someFiles, err := filepath.Glob(filepath.Join(path, ext))
-		log.Debugf("glob: %v", filepath.Join(path, ext))
-		log.Debugf("pictures files: %v", someFiles)
+	for _, ext := range pictureExtList {
+		fileList, err := filepath.Glob(filepath.Join(picturePath, ext))
 		if err != nil {
-			log.Warningf("Unable to find pictures: %v", err)
+			log.Warningf("Unable to launch a search with \"%s\" extension", ext)
 		}
 
-		for _, onePicture := range someFiles {
-			pictureList = append(pictureList, onePicture)
+		for _, fn := range fileList {
+			picturesList = append(picturesList, fn)
 		}
 	}
 
-	natsort.Strings(pictureList)
-	log.Debugf("pictureList: %v", pictureList)
+	natsort.Strings(picturesList)
 
-	return pictureList
+	return picturesList
 }
 
-func launchShipping(pool *email.Pool, ch chan piece, removeChan chan<- string) {
-	for i := 0; i < 2; i++ {
-		go func(num int, pool *email.Pool, ch chan piece, removeChan chan<- string) {
-			log := logging.MustGetLogger("log")
-
-			timeout := 10 * time.Second
-
-			for e := range ch {
-				err := pool.Send(e.Email, timeout)
-				if err != nil {
-					errSplitted := strings.Split(err.Error(), " ")
-
-					switch errSplitted[0] {
-					case "421":
-						ch <- e
-						time.Sleep(120 * time.Second)
-					case "550":
-						fmt.Println("I can't send email anymore !")
-						os.Exit(1)
-					}
-				} else {
-					log.Debug("Email was sent")
-
-					removeChan <- e.Picture
-				}
-			}
-		}(i, pool, ch, removeChan)
-	}
-}
-
-func moveFiles(pictureList *[]string, newPath *string) {
+func moveFilesAndChangePath(picturesList *[]string, newPath *string) *[]string {
 	log := logging.MustGetLogger("log")
+	newPathList := make([]string, len(*picturesList))
 
-	for _, pic := range *pictureList {
+	for num, pic := range *picturesList {
 		_, filename := path.Split(pic)
 		np := path.Join(*newPath, filename)
 		if err := os.Rename(pic, np); err != nil {
@@ -140,72 +102,111 @@ func moveFiles(pictureList *[]string, newPath *string) {
 		} else {
 			log.Debugf("\"%s\" was moved to \"%s\"", pic, np)
 		}
+
+		newPathList[num] = np
 	}
+
+	return &newPathList
 }
 
-func removePicture(removeChan <-chan string) {
-	log := logging.MustGetLogger("log")
+func shippingEmail(pool *email.Pool, ch chan attachment, removePic chan<- string) {
+	for i := 0; i < 2; i++ {
+		go func(num int, pool *email.Pool, ch chan attachment, removePic chan<- string) {
+			log := logging.MustGetLogger("log")
 
-	for true {
-		picture := <-removeChan
-		os.Remove(picture)
+			timeout := 10 * time.Second
 
-		log.Debugf("Picture \"%s\" was removed", picture)
+			for e := range ch {
+				if err := pool.Send(e.email, timeout); err != nil {
+					errSplitted := strings.Split(err.Error(), " ")
+					log.Debugf("Error: %s", err)
+
+					switch errSplitted[0] {
+					case "421":
+						ch <- e
+						log.Debug("Waiting 2 minutes since too many email have been sent !")
+						time.Sleep(2 * time.Minute)
+					case "550":
+						log.Debug("Oops, I can't send email anymore !")
+						os.Exit(1)
+					case "dial":
+						log.Debug("Oops, connexion refused !")
+						os.Exit(1)
+					}
+				} else {
+					log.Debug("Email was sent")
+
+					removePic <- e.pictureName
+				}
+			}
+		}(i, pool, ch, removePic)
 	}
 }
 
 func startApp() {
 	log := logging.MustGetLogger("log")
 
-	emailChan := make(chan piece, 80)
-	removeChan := make(chan string, 80)
-
 	mySMTP := viper.GetString("email.smtp")
-	host := fmt.Sprintf("%s:%d", mySMTP, viper.GetInt("email.port"))
 	login := viper.GetString("email.login")
 	password := viper.GetString("email.password")
-	sleepBefore := viper.GetInt("default.sleepBeforeStarting")
-	sleep := time.Duration(viper.GetInt("default.sleepTime")) * time.Second
+	host := fmt.Sprintf("%s:%d", mySMTP, viper.GetInt("email.port"))
+	sleepBeforeStart := viper.GetInt("default.sleepBeforeStarting")
+	sleepBetweenLoop := viper.GetInt("default.sleepTime")
 	defaultPath := viper.GetString("default.picturePath")
 	newPath := path.Join(defaultPath, "send")
 
-	log.Debugf("Time waiting between 2 loops: %.0fs", sleep.Seconds())
+	removePictureChan := make(chan string, 80)
+	emailChan := make(chan attachment, 80)
 
 	createPath(&newPath)
 
+	log.Debugf("Before starting to work, waiting \"%ds\"", sleepBeforeStart)
+	time.Sleep(time.Duration(sleepBeforeStart) * time.Second)
+	log.Debug("Removing all pictures before starting infinit loop")
+
 	pool := email.NewPool(host, 4, smtp.PlainAuth("", login, password, mySMTP))
-	// Start remove picture goroutine
-	go removePicture(removeChan)
 
-	log.Debugf("Before starting to work, waiting \"%ds\"", sleepBefore)
-	// Sleep before starting
-	time.Sleep(time.Duration(sleepBefore) * time.Second)
+	picturesList := findPicture()
+	log.Debugf("picturesList: %v", picturesList)
+	removePictures(&picturesList)
 
-	log.Debug("After sleeping at the beginning")
-	log.Debug("Removing all pictures before starting")
+	go removePictureGoroutine(removePictureChan)
 
-	// Remove picture before starting
-	pictureList := findPicture()
-	for _, picture := range pictureList {
-		removeChan <- picture
-	}
+	shippingEmail(pool, emailChan, removePictureChan)
 
-	launchShipping(pool, emailChan, removeChan)
-	time.Sleep(sleep)
-
+	log.Debug("Starting infinit loop")
 	for {
-		pictureList = findPicture()
-		moveFiles(&pictureList, &newPath)
-		changePathList(&pictureList, &newPath)
+		picturesList = findPicture()
+		log.Debugf("picturesList: %v", picturesList)
+		newPicturesListPtr := moveFilesAndChangePath(&picturesList, &newPath)
 
-		for _, pic := range pictureList {
-			e := createEmail(&pic)
-			emailChan <- piece{
-				Email:   e,
-				Picture: pic,
+		for _, pic := range *newPicturesListPtr {
+			e := createAnEmail(&pic)
+			emailChan <- attachment{
+				email:       e,
+				pictureName: pic,
 			}
 		}
-		// wait before start another pictures search
-		time.Sleep(sleep)
+		log.Debugf("I'll sleep during %d second(s)", sleepBetweenLoop)
+		time.Sleep(time.Duration(sleepBetweenLoop) * time.Second)
+	}
+}
+
+func removePictureGoroutine(removePic <-chan string) {
+	log := logging.MustGetLogger("log")
+
+	for pictureFilename := range removePic {
+		os.Remove(pictureFilename)
+
+		log.Debugf("Picture \"%s\" was removed", pictureFilename)
+	}
+}
+
+func removePictures(picturesList *[]string) {
+	log := logging.MustGetLogger("log")
+
+	for _, pic := range *picturesList {
+		os.Remove(pic)
+		log.Debugf("Picture \"%s\" was removed", pic)
 	}
 }
